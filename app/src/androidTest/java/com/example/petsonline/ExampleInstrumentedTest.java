0c8c7c252bfa3ff2c84e8c252de4170b05f07b80
SHAHZAIB <shahzaiba9872@gmail.com>
2022-03-16 03:36:18 +0500

    1st Commit

--- app/src/androidTest/java/com/example/petsonline/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/petsonline/ExampleInstrumentedTest.java
@@ -1,10 +1,11 @@
-package com.petsonline;
+package com.example.petsonline;
 
 import android.content.Context;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
--- app/src/test/java/com/example/ExampleUnitTest.java
+++ app/src/test/java/com/example/ExampleUnitTest.java
@@ -1,6 +1,6 @@
-package com.petsonline;
+package com.example;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import static org.junit.Assert.*;
 
